171301469e0c622c2e8224170fa8ea325967717e
dpwl0974 <67728547+dpwl0974@users.noreply.github.com>
2024-10-10 16:03:10 +0900

    Fix: 충돌 해결

--- src/server/src/main/java/chugpuff/chugpuff/domain/AIInterviewFF.java
+++ src/server/src/main/java/chugpuff/chugpuff/domain/AIInterviewFF.java
@@ -1,9 +1,6 @@
 package chugpuff.chugpuff.domain;
 
-<<<<<<< HEAD
-=======
 import com.fasterxml.jackson.annotation.JsonBackReference;
->>>>>>> ca63ab59f84b3bee18722590476cbe8f39143013
 import jakarta.persistence.*;
 import lombok.*;
 
@@ -16,23 +13,11 @@ public class AIInterviewFF {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long AIInterviewFFNo;
 
-<<<<<<< HEAD
-    @OneToOne
-    @JoinColumn(name = "AIInterviewNo")
-=======
     @ManyToOne
     @JoinColumn(name = "AIInterviewNo")
     @JsonBackReference
->>>>>>> ca63ab59f84b3bee18722590476cbe8f39143013
     private AIInterview aiInterview;
 
     private String f_question;
     private String f_answer;
-<<<<<<< HEAD
-
-    @Column(columnDefinition = "LONGTEXT")
-    private String f_feedback;
 }
-=======
-}
->>>>>>> ca63ab59f84b3bee18722590476cbe8f39143013
--- src/server/src/main/java/chugpuff/chugpuff/domain/AIInterviewIF.java
+++ src/server/src/main/java/chugpuff/chugpuff/domain/AIInterviewIF.java
@@ -1,9 +1,6 @@
 package chugpuff.chugpuff.domain;
 
-<<<<<<< HEAD
-=======
 import com.fasterxml.jackson.annotation.JsonBackReference;
->>>>>>> ca63ab59f84b3bee18722590476cbe8f39143013
 import jakarta.persistence.*;
 import lombok.*;
 
@@ -18,10 +15,7 @@ public class AIInterviewIF {
 
     @ManyToOne
     @JoinColumn(name = "AIInterviewNo")
-<<<<<<< HEAD
-=======
     @JsonBackReference
->>>>>>> ca63ab59f84b3bee18722590476cbe8f39143013
     private AIInterview aiInterview;
 
     private String i_question;
@@ -29,8 +23,4 @@ public class AIInterviewIF {
 
     @Column(columnDefinition = "LONGTEXT")
     private String i_feedback;
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> ca63ab59f84b3bee18722590476cbe8f39143013
